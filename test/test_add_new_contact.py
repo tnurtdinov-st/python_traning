fc705137dc414208dd48995902ab33ed2ea30867
tnurtdinov <tnurtdinov91@gmail.com>
2021-04-25 18:07:03 +0300

    Урок 6 задание 19

--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -2,7 +2,8 @@
 from model.contact import Contact
 
 
-def test_main(app, contact):
+def test_main(app, json_contact):
+    contact = json_contact
     old_contact = app.contact.get_contact_list()
     app.contact.add_new_contact()
     app.contact.fill_data(contact)
